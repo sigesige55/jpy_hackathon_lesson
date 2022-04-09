ebf6a4fdb33085fc79d3e9131fa7d022a8a42d8a
AozoraHiroshi <sakofu128@gmail.com>
2022-04-10 07:50:31 +0900

    Create index_org.js

---
+++ index_org.js
@@ -0,0 +1,9 @@
+//雛形；関数内の処理を全部待ってから画面を表示する
+window.onload = async function() {
+  
+  //メタマスクに接続してブロック番号を取ってくる
+  const provider = await new ethers.provider.Web3Provider(window.ethereum);
+
+  
+
+}
