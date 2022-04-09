75b898686dfc8b03e01ac7314dc68751a0c768ad
AozoraHiroshi <sakofu128@gmail.com>
2022-04-10 07:57:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ window.onload = async function() {
   const provider = await new ethers.providers.Web3Provider(window.ethereum);
   const blocknumber = await provider.getBlockNumber();
   alert(blocknumber);
-  
+  //サイナーのアドレス取得
+  address = await ethereum.request({ method: 'eth_requestAccounts' });
+  const signer = await provider.getSigner();
+  alert(address[0]);
 
 }
 
